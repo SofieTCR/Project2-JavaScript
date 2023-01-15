71d24c607c86d03a1e42a14dbb72808b116e58ee
Jack-van-Bommel <9019233@student.zadkine.nl>
2023-01-15 15:15:38 +0100

    asdasdasd

---
+++ Webpage/JackGame/JackGscript.js
@@ -0,0 +1,12 @@
+var GameArea = document.getElementById("gamearea");
+var ctx = GameArea.getContext("2d");
+var CharIMG = new Image();
+
+function StartGame() {
+    CharIMG.src = "./assets/JG_character.png";
+}
+
+function DrawChar() {
+    
+}
+
